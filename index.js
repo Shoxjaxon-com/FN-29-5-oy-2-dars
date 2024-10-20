2324c132fb3fdcc1f9a96da6dafc76e825d3363a
Shoxjaxon-com <alijonovshohjahon03@gmail.com>
2024-10-19 23:28:59 -0700

    vazifa

---
+++ index.js
@@ -0,0 +1,128 @@
+//  === Massivlarga oid == //
+
+//     1 - masala
+
+// let arr = ['apple', 'car', 'banana', 'tree', 'strawberry']
+// function soz(arr) {
+//     return arr
+//         .filter(arr => arr.length >= 5)
+//         .map(arr => arr.charAt(0).toUpperCase() + arr.slice(1))
+// }
+// console.log(soz(arr));
+
+// 2 - masala
+
+// const talaba = [
+//     { name: 'Mirzatillo', age: 23, },
+//     { name: 'Shohjahon', age: 16, },
+//     { name: 'Dilhushbek', age: 15, },
+// ]
+// function talabalar(talaba) {
+//     const studen = talaba.find(talaba => talaba.age >= 20)
+//     return studen.name.toUpperCase()
+// }
+// console.log(talabalar(talaba));
+
+// 3 - masala
+
+// const arr = [10000, 12000, 5000]
+// function sum(arr) {
+//     return arr
+//         .filter(summa => summa > 10000)
+//         .reduce((total, summa) => total + summa)
+// }
+// console.log(sum(arr));
+
+
+// 4 - masala
+
+// function sum(arr) {
+//     if (arr.every(arr => arr > 0)) {
+//         console.log('Sonlar musbat');
+//     } else {
+//         console.log('Manfiy son');
+//     }
+
+//     if (arr.some(arr => arr < 100)) {
+//         console.log('Katta son bor');
+//     }
+//     return arr
+// }
+// // console.log(sum(arr));
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// sum(arr);
+
+// 5 - masala
+
+// function kavadrat(arr) {
+//     return arr.map(function (a) {
+//         return a * a;
+//     });
+// }
+
+// function sum(arr) {
+//     arr.forEach(function (b) {
+//         console.log(b);
+//     });
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let res = kavadrat(arr);
+// sum(res);
+
+// 6 - masala
+
+// let arr = [1000, 10000, 12000]
+// function maosh(arr) {
+//     return arr
+//         .filter(function (v) {
+//             return v <= 2000
+//         })
+//         .map(function (a) {
+//             return a * 1.10
+//         })
+//         .reduce(function (sum, a) {
+//             return a + sum
+//         })
+
+// }
+// console.log(maosh(arr));
+
+// === Yozuvga oid == //
+
+//     1 - masala
+
+// let str = 'salom bollalar '
+// let res = str.toUpperCase()
+// console.log(res);
+
+// 2 - masala
+
+// let arr = 'JavaScript ni men organishni boshladim'
+// let word = 'men'
+// function soz(arr, word) {
+//     return arr.includes(word)
+// }
+
+// console.log(soz(arr, word));
+
+// 3 - masala
+
+// let str = 'JavaScript'
+// function soz(str) {
+//     return str.replace(/JavaScript/g, 'JS')
+// }
+// console.log(soz(str));
+
+// 4 - masala
+
+// let arr = 'salom'
+// function soz(arr) {
+//     return arr.split('').reverse().join('')
+// }
+// console.log(soz(arr));
+
+
+console.log(z);
+let z =10;
+console.log(z);
